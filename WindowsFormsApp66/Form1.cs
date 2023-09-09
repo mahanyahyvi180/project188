e3cafca10baeb1f1a2099d4d474ff5b7e6407334
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-09-09 22:53:38 +0330

    Add files via upload

---
+++ WindowsFormsApp66/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp66
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s1, s2;
+            s1 = "ali";
+            s2 = s1;
+            s1 = "sanaz";
+            label1.Text = s2;
+        }
+    }
+}
